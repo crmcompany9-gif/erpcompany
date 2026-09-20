ceb5b08262deed2f0394fb1b24c99de05e087588
Jitesh Soni <jiteshsoni3591@gmail.com>
2026-09-20 23:19:09 +0530

    Fix new Express version

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ const allowedOrigins = [
   'https://erpcompany.onrender.com',
   'https://erpportal-lqux.onrender.com',
 ];
-app.options('*', cors());
+
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
